d8615288fd3fff12b059f1bdfcd3ae3b79cc90a2
Deathcoald <koncevich13@mail.ru>
2025-03-18 19:44:10 +0300

    central button

--- build_map.py
+++ build_map.py
@@ -14,6 +14,8 @@ def extract_state(file_name):
 
 def build_states(state_data, tweets):
     fig, ax = plt.subplots(figsize=(8, 6))
+    state_x = []
+    state_y = []
 
     for state, polygons in state_data.items():
         for polygon in polygons:
@@ -22,7 +24,16 @@ def build_states(state_data, tweets):
             
             x = [points[0] for points in polygon]
             y = [points[1] for points in polygon]
+
+            state_x.extend(x)
+            state_y.extend(y)
+
             ax.plot(x, y, label=state if len(ax.lines) == 0 else "", color="black")
+
+        if state_x and state_y:
+            central_point_x = sum(state_x) / len(state_x)
+            central_point_y = sum(state_y) / len(state_y)
+            ax.plot(central_point_x, central_point_y, color="red", marker="o", markersize=5)
 
     ax.legend()
     ax.set_xlabel("Долгота")
@@ -36,12 +47,7 @@ def build_states(state_data, tweets):
 def tweets_points(tweets, ax):
     
     for tweet in tweets:
-        x = float(tweet.latitude)
-        y = float(tweet.longitude)
+        x = float(tweet.longitude)
+        y = float(tweet.latitude)
         ax.plot(x, y, color="black", marker="o", markersize=5)
-    
-    ax.legend(["Tweet Locations"])
-    ax.set_xlabel("Долгота")
-    ax.set_ylabel("Ширина")
-    ax.set_title("Штаты")
 
