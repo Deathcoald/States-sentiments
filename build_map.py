cd3e6db833e9ba1afbf224f8c26b60ebdcc96ee1
Deathcoald <koncevich13@mail.ru>
2025-03-18 17:11:14 +0300

    draw

--- build_map.py
+++ build_map.py
@@ -2,9 +2,9 @@ import json
 import matplotlib.pyplot as plt
 
 
-def built_map(file_name = "Data/states.json"):
+def build_map(tweets, file_name = "Data/states.json"):
     state_data = extract_state(file_name)
-    build_states(state_data)
+    build_states(state_data, tweets)
 
 
 def extract_state(file_name):
@@ -12,24 +12,36 @@ def extract_state(file_name):
         return json.load(states)
 
 
-def build_states(state_data):
+def build_states(state_data, tweets):
     fig, ax = plt.subplots(figsize=(8, 6))
 
     for state, polygons in state_data.items():
         for polygon in polygons:
             print(polygon)
             print(state)
+            
             x = [points[0] for points in polygon]
             y = [points[1] for points in polygon]
-            ax.plot(x, y, label=state if len(ax.lines) == 0 else "")
+            ax.plot(x, y, label=state if len(ax.lines) == 0 else "", color="black")
 
     ax.legend()
     ax.set_xlabel("Долгота")
     ax.set_ylabel("Ширина")
     ax.set_title("Штаты")
-    plt.show(block=True)
 
+    tweets_points(tweets, ax)
 
-built_map()
+    plt.show()
 
+def tweets_points(tweets, ax):
+    
+    for tweet in tweets:
+        x = float(tweet.latitude)
+        y = float(tweet.longitude)
+        ax.plot(x, y, color="black", marker="o", markersize=5)
+    
+    ax.legend(["Tweet Locations"])
+    ax.set_xlabel("Долгота")
+    ax.set_ylabel("Ширина")
+    ax.set_title("Штаты")
 
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import build_map
 
 tweets = tweet_parser.extract_words("Data/cali_tweets2014.txt")
 
-build_map()
+build_map.build_map(tweets)
